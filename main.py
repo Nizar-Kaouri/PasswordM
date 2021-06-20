e4bcaeccb35bca4c4f9ae52d472a945143254bce
Nizar-Kaouri <p.kaouri@gmail.com>
2021-06-20 15:51:41 +0200

    Initial Commit

--- main.py
+++ main.py
@@ -1,106 +1,141 @@
-import random                                                                                                  # Module um zufälligen chars to generieren
-import datetime                                                                                                # Module um die Timestamps zu speichern
-import pyperclip                                                                                               # Module um Passwords zu copieren
-import stdiomask as stdiomask                                                                                  # Module um das Master-Password nicht im KLartext zu zeigen
-import typer                                                                                                   # Module für die Kommandozeilen in CMD
-import string                                                                                                  # Module enthält die benötigte Buchstaben/Zahlen/Symbole
-import os                                                                                                      # System Module um die txt Files zu checken/löschen
+import random
+import datetime
+import pyperclip
+import stdiomask as stdiomask
+import typer
+import string
+import os
 
-app = typer.Typer()                                                                                            # Wir definieren ein Typer Objekt für die Kommandozeilen
+app = typer.Typer()
 
-master_password = "admin"                                                                                      # Ein Master Password, das nur der Benutzer kennt.
+master_password = "admin"
 
-def generator(password_length):                                                                                # Function um ein Password zu generieren
-    lower = string.ascii_lowercase                                                                             # Das Password kann Groß/Klein-Buchstaben, Zahlen und Zeichen enthalten
+def encrypt(data, shift):
+    encrypted = ""
+    for i in range (len(data)):
+        char = data[i]
+        if (char.isupper()):
+            encrypted += chr((ord(char) + shift - 65) % 26 + 65 )
+        elif (char.islower()):
+            encrypted += chr((ord(char) + shift - 97) % 26 + 97 )
+        elif (char.isdigit()):
+            number = (int(char) + shift) % 10
+            encrypted += str(number)
+        else:
+            encrypted += char
+    return encrypted
+
+def decrypt(data, shift):
+    decrypted = ""
+    for i in range (len(data)):
+        char = data[i]
+        if (char.isupper()):
+            decrypted += chr((ord(char) - shift - 65) % 26 + 65 )
+        elif (char.islower()):
+            decrypted += chr((ord(char) - shift - 97) % 26 + 97 )
+        elif (char.isdigit()):
+            number = (int(char) - shift) % 10
+            decrypted += str(number)
+        else:
+            decrypted += char
+    return decrypted
+
+def generator(password_length):
+    lower = string.ascii_lowercase
     upper = string.ascii_uppercase
     numbers = string.digits
     symbols = string.punctuation
     password = lower + upper + numbers + symbols
-    temp = random.sample(password, password_length)                                                             # Die Character in einer zufälliger Weise generieren, und dann
-    password = "".join(temp)                                                                                    # in einem String zusammen addieren
+    temp = random.sample(password, password_length)
+    password = "".join(temp)
     return password
 
-def copieren(x):                                                                                                # Function um ein Password von einem txt File zu copieren,
-    file = open("passwords.txt", 'r')                                                                           # dadurch kann man den Benutzernamen zeigen und password copieren.
+def copieren(x):
+    shift = 5
+    file = open("passwords.txt", 'r')
     zeilen = file.readlines()
     for rows in zeilen:
-        column = rows.split('|')                                                                                # Loop um den Titel und das entsprechende Password zu finden
-        titel = column[0]
+        column = rows.split('|')
+        titel = decrypt(column[0],shift)
         if x.lower() in titel.lower():
-            print("Benutzername:" + column[1])
-            pyperclip.copy(column[2][column[2].index('')+1:])                                                   # Password copiert
+            print("Benutzername:" + decrypt(column[1],shift))
+            pyperclip.copy(decrypt(column[2][column[2].index('')+1:],shift))
             print("Password copied to clipboard.")
 
     file.close()
 
-@app.command()                                                                                                  #       add + Titel + Benutzername + Länge des Passwortes
+@app.command()
 def add(titel: str, name: str, password_length: int):
+    shift =  5
     file = open("passwords.txt", 'a')
-    password = generator(password_length)                                                                       # Ein Password mit der Generator- Function erstellen
-    pyperclip.copy(password)                                                                                    # Das Password zum Clipboard copieren
-    file.write(titel + " | " + name + " | " + password + "\n")                                                  # Das Password in einem txt File mit Titel und Benutzername speichern
+    password = generator(password_length)
+    pyperclip.copy(password)
+    file.write(encrypt(titel,shift) + " | " + encrypt(name,shift) + " | " + encrypt(password,shift) + "\n")
     print("Password generated and copied to Clipboard")
     file.close()
 
-@app.command()                                                                                                  # display
+@app.command()
 def display():
-    master_password_input = stdiomask.getpass()                                                                 # Der Benutzer wird nach einem Master-Password gefragt
-    if master_password_input == master_password:                                                                # Wenn richtig eingegeben , werden alle Passwörter im Bildschirm gezeigt
-        file = open("passwords.txt", 'r')                                                                       # Format : TITEL | BENUTZERNAME | PASSWORD
-        text = file.read()
-        print(text)
+    shift = 5
+    master_password_input = stdiomask.getpass()
+    if master_password_input == master_password:
+        file = open("passwords.txt", 'r')
+        for i in file:
+            data = i.split("|")
+            print(decrypt(data[0],shift) + "|"+ decrypt(data[1],shift) + "|"+ decrypt(data[2],shift))
         file.close()
     else:
-        print("Access Denied . Wrong Master Password. ")                                                        # wenn falsch eingegeben --> Ablehnungsnachricht
+        print("Access Denied . Wrong Master Password. ")
 
-@app.command()                                                                                                  #        delete + TITEL
+@app.command()
 def delete(x):
-    with open("passwords.txt", "r+") as file:                                                                   # Nach einer Bestätigung fragen
+    shift = 5
+    with open("passwords.txt", "r+") as file:
         print("Press Y for Yes or else for No.")
         choice = input("Do you really want to delete: " + str(x) +"\n")
-        if choice == "y":                                                                                       # Wenn Ja wird die Ziele, die den Titel enthält, gelöscht
+        if choice == "y":
             lines = file.readlines()
             file.seek(0)
             for line in lines:
-                if x not in line:
+                if encrypt(x,shift) not in line:
                     file.write(line)
             file.truncate()
             print("Done.")
         else:
-            print("Cancelled.")                                                                                 # Wenn nicht, abgesagen
+            print("Cancelled.")
 
-@app.command()                                                                                                  #        copy + TITEL
+@app.command()
 def copy(x):
-    if not os.path.isfile("timestamp.txt"):                                                                     # Es wird geprüft ob ein file mit Passwörtern schon existiert
-        timefile = open("timestamp.txt", 'w')                                                                   # Wenn keins existiert, wird eins ertstellt
-        timefile.write("2021-06-11 18:42:16.058009")                                                            # Sonst wird diese If-Anweisung übersprungen
+    if not os.path.isfile("timestamp.txt"):
+        timefile = open("timestamp.txt", 'w')
+        timefile.write("2021-06-11 18:42:16.058009")
         timefile.close()
     timefile = open("timestamp.txt", 'r')
-    timestampstr = timefile.readline()                                                                          # Anfangs-Timestamp
-    timestamp = datetime.datetime.strptime(timestampstr,"%Y-%m-%d %H:%M:%S.%f")                                 # Anfangs-Timestamp speichern
-    timedelta = datetime.datetime.now() - timestamp                                                             # Differenz zwischen Anfangs-Timestamp und NOW-Timestamp
-    if timedelta > datetime.timedelta(minutes=1):                                                               # Wenn die Differenz größer als die erlaubte periode ist,
-        master_password_input = stdiomask.getpass()                                                             # wird es nach einem Master-Password gefragt
+    timestampstr = timefile.readline()
+    timestamp = datetime.datetime.strptime(timestampstr,"%Y-%m-%d %H:%M:%S.%f")
+    timedelta = datetime.datetime.now() - timestamp
+    if timedelta > datetime.timedelta(minutes=1):
+        master_password_input = stdiomask.getpass()
         if master_password == master_password_input:
-            timefile = open("timestamp.txt", 'w')
-            timefile.write(str(datetime.datetime.now()))                                                        # wird ein neues NOW-Timestamp ertstellt
-            copieren(x)                                                                                         # und dann die Copieren-Fuction laufen lassen
+            timefile = open("timestamp1.txt", 'w')
+            timefile.write(str(datetime.datetime.now()))
+            copieren(x)
         else:
             print("Access denied.")
     else:
-        timefile = open("timestamp.txt", 'w')                                                                   # wenn die Differenz kleiner als die erlaubte periode ist
-        timefile.write(str(datetime.datetime.now()))                                                            # wird ein neues NOW-Timestamp ertstellt
-        copieren(x)                                                                                             # und dann die Copieren-Fuction laufen lassen
+        timefile = open("timestamp1.txt", 'w')
+        timefile.write(str(datetime.datetime.now()))
+        copieren(x)
 
-@app.command()                                                                                                  #        clear
+@app.command()
 def clear():
-    print("Press Y for Yes or else for No.")                                                                    # Nach einer Bestätigung fragen
+    print("Press Y for Yes or else for No.")
     choice = input("Do you really want to clear all your passwords ? \n ")
-    if choice == "y":                                                                                           # Wenn Ja, das txt File löschen
+    if choice == "y":
         os.remove("passwords.txt")
         print("All passwords cleared.")
     else:
-        print("Canceled.")                                                                                      # Wenn nicht --> absagen
+        print("Canceled.")
 
 if __name__ == "__main__":
     app()
